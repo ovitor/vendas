df860e248d66f4dd4341714584ae16ad3e094d18
Vitor Carvalho <vcml@pm.me>
2021-01-03 16:10:12 -0300

    first commit

---
+++ components/product.vue
@@ -0,0 +1,28 @@
+<template>
+</template>
+<script>
+export default {
+  name: 'product',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      products: [
+      {
+        title: '',
+        description: '',
+        price: '',
+        image: '',
+        status: '',
+        condition: '',
+      },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
